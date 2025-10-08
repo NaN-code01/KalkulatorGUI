02544712a122a6211b73e0be298c7d95c40a731a
NaN <afnan.martan13@gmail.com>
2025-10-08 17:10:56 +0700

    nambah app.maknloop, fungsi buat_tombol (blm selesai)

--- main.py
+++ main.py
@@ -26,11 +26,30 @@ class KalkulatorGUI(CT.CTk):
     # ekspresi matematika awal dan font awal
     self.ekspresi_matematika = "0"
     self.ukuran_font = 64
+  
+  def buat_tombol(self):
     
-    self.create_widgets()
+    # frame tampilan utama -> (bagian atas kalkulator)
+    frame_tampilan = CT.CTkFrame(self, fg_color=self.bg_color)
+    frame_tampilan.pack(fill="x", padx=20, pady=(40, 10))
+    
+    # tanda [ = ] -> (di bagian tampilan)
+    tanda_samaDengan = CT.CTkLabel(frame_tampilan, text="=",
+                       font=("Arial", 45), text_color=self.accent_color)
+    tanda_samaDengan.pack(side="left", padx=(0, 10))
+    
+    # tampilan hasil
+    self.hasil = CT.CTkEntry(frame_tampilan, font=("Arial", 64),
+                             fg_color=self.bg_color, text_color="white",
+                             border_width=0, justify="right")
+    # jadiin read-only -> (cuma buat nampilin)
+    self.hasil.pack(fill="x", expand=True)
+    self.hasil.insert(0, "0")
+    self.hasil.configure(state="readonly")
 
 def main() -> None:
-  pass
+  app = KalkulatorGUI()
+  app.mainloop()
 
 if __name__ == "__main__":
   main()
