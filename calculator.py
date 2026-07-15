d8f42f7bfa6e239c314850d19dd93412e1f74160
NaN-code01 <afnan.martan13@gmail.com>
2026-07-15 17:58:59 +0700

    add postfix evaluation

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import re
+import math
 from typing import TypedDict
 
 class OperatorInfo(TypedDict):
@@ -30,6 +31,7 @@ class Calculator:
     return token_pattern.findall(expression)
 
   def infix_to_postfix(self, tokens: list[str]) -> list[str]:
+    operators: dict = self.operators
     output_queue: list[str] = []
     operator_stack: list[str] = []
 
@@ -52,12 +54,12 @@ class Calculator:
           operator_stack.pop()
       
       # operator handling
-      elif token in self.operators:
-        token_prec: int = self.operators[token]["prec"]
-        token_assoc: str = self.operators[token]["assoc"] 
+      elif token in operators:
+        token_prec: int = operators[token]["prec"]
+        token_assoc: str = operators[token]["assoc"] 
         
         while operator_stack and operator_stack[-1] != "(":
-          operator_prec: int = self.operators[operator_stack[-1]]["prec"]
+          operator_prec: int = operators[operator_stack[-1]]["prec"]
           
           # Apply precedence and associativity rules
           if (
@@ -76,8 +78,33 @@ class Calculator:
     
     return output_queue
 
-  def evaluate_postfix(self) -> float:
-    pass
+  def evaluate_postfix(self, postfix: list[str]) -> str:
+    stack: list[float] = []
+    
+    for token in postfix:
+      if token.replace(".", "", 1).isdigit():
+        stack.append(float(token))
+      
+      else:
+        value1: float = stack.pop()
+        value2: float = stack.pop()
+
+        match(token):
+          # value2 then value1 order following postfix expression
+          case "+": stack.append(value2 + value1)
+          case "-": stack.append(value2 - value1)
+          case "*": stack.append(value2 * value1)
+          case "/": stack.append(value2 / value1)
+          case "^": stack.append(value2 ** value1)
+          case _: stack.append(float(0))
+    
+    result: float = stack.pop()
+
+    # return str formated int if the float value is int
+    if result.is_integer():
+      return str(int(result))
+    
+    return str(result)
 
 def main() -> None:
   pass
