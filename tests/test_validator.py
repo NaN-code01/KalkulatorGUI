92c583b72fcf3ff41a58d21d285b08f851e65678
NaN-code01 <afnan.martan13@gmail.com>
2026-08-07 11:49:41 +0700

    add test for validate_tokens method

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -2,6 +2,7 @@ import pytest
 
 from modules.validator import Validator
 
+
 # Test validate_expression() ------------------------------
 
 @pytest.mark.parametrize("expression", [
@@ -55,10 +56,10 @@ def test_tokens_lexical_check_success(tokens, expression):
   assert Validator.tokens_lexical_check(tokens, expression)
 
 @pytest.mark.parametrize("tokens, expression", [
-  (["0"],           "0 "  ),
-  (["1", "+"],      "1 +" ),
-  (["2", "3"],      "-23" ),
-  (["^", "4", ")"], "^)"  ),
+  (["0"],           "0 " ),
+  (["1", "+"],      "1 +"),
+  (["2", "3"],      "-23"),
+  (["^", "4", ")"], "^)" ),
 ])
 def test_tokens_lexical_check_invalid(tokens, expression):
   with pytest.raises(
@@ -66,3 +67,23 @@ def test_tokens_lexical_check_invalid(tokens, expression):
     match="Expression contain an invalid token"
   ):
     Validator.tokens_lexical_check(tokens, expression)
+
+
+
+
+@pytest.mark.parametrize("tokens", [
+  (["0"]),
+  (["1", "+", "2"]),
+  (["3", "-", "(", "4", "*", "5", ")", "/", "u-", "6"]),
+])
+def test_validate_tokens_success(tokens):
+  assert Validator.validate_tokens(tokens)
+
+@pytest.mark.parametrize("failed_tokens", [
+  (["+", ")", "-", "(", "u+"]),
+  (["(", "0", "(", ")"]),
+  (["1", "u-", "*", "/", ")", "2.3"]),
+])
+def test_validate_tokens_failed(failed_tokens):
+  with pytest.raises(ValueError):
+    Validator.validate_tokens(tokens=failed_tokens)
